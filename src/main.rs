b764ad70216463acda72ae2e8e44967fef0bcb06
Christian Gati <christian.gati@gmail.com>
2014-11-09 14:56:58 -0500

    Added value and get_next methods.
    get_next needs work. Need a way of getting “tail” as either a reference
    or itself out of &self.

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,22 @@ impl Node {
         Cons(elem, box self)
     }
 
+    fn value(&self) -> uint {
+        match *self {
+            Cons(x, _) => x,
+            Nil => 0
+        }
+    }
+
+    // FIXME: Any node you pass in transfers ownership. Need to figure out how
+    // to pull a reference to tail out of &self.
+    fn get_next(self) -> Node {
+        match self {
+            Cons(_, tail) => *tail,
+            Nil => self
+        }
+    }
+
     fn contains(&self, elem: uint) -> bool {
         match *self {
             Cons(x, ref tail) => if x == elem { true } else { tail.contains(elem) },
@@ -43,8 +59,14 @@ impl Node {
 fn main() {
     let mut head: Node = Cons(1, box Nil);
     head = head.append_to_front(2);
+    head = head.append_to_front(3);
 
     println!("The length of the linked list is: {}", head.length());
     print!("The linked list contains: ");
     head.print_list();
+
+    let mut val = head.contains(10);
+    let mut val2 = head.contains(2);
+
+    println!("List contains 10? {}. List contains 2? {}", val, val2);
 }
